e40a4f28ec7811eaf3ab03447f6f1e90220f25ed
Admin <Admin@DESKTOP-B5L4A4H>
2023-03-12 22:54:07 +0530

    commite data

---
+++ src/GithUb/Addition_OfTwo_Number.java
@@ -0,0 +1,13 @@
+package GithUb;
+
+public class Addition_OfTwo_Number 
+{
+  public static void main(String[] args) 
+  {
+	int a=10;
+  int  b=20;
+  int c;
+  c=a+b;
+  System.out.println(c);
+}
+}
